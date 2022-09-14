9833c5e60cea71139ae610062441f726da02e8c4
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2022-09-14 16:10:17 -0400

    Project almost completed
    JavaScript added and project almost completed.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,31 @@
-// Call helloWorld
-function helloWorld() {
-    alert("Hello World");
+//Get the string from the textbox
+//controller function
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let reversedString = reverseString(userString);
+    displayString(reversedString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString) {
+    let reversedString = [];
+
+    //reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        reversedString += userString[i];
+    }
+
+    return reversedString;
+}
+
+//Display the reversed string
+//view function
+function displayString(reversedString) {
+
+    //Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${reversedString}`;
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
